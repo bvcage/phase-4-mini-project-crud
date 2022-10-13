a42308f2bae1900b4885d41ea7fed2f861c350b2
Bailey Cage <bailey.cage@gmail.com>
2022-10-13 14:32:52 -0400

    commit tree 532ff6f08908fdeebb069820be2ffd291932d4bb

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,40 @@
+class SpicesController < ApplicationController
+   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+   def index
+      spices = Spice.all
+      render json: spices, status: :ok
+   end
+
+   def create
+      spice = Spice.create(spice_params)
+      render json: spice, status: :created
+   end
+
+   def update
+      spice = find_spice
+      spice.update(spice_params)
+      render json: spice, status: :accepted
+   end
+
+   def destroy
+      spice = find_spice
+      spice.destroy
+      head :no_content
+   end
+
+   private
+
+   def spice_params
+      params.permit(:title, :image, :description, :notes, :rating)
+   end
+
+   def find_spice
+      Spice.find_by(id: params[:id])
+   end
+
+   def render_not_found_response
+      render json: { error: "no spice found" }, status: :not_found
+   end
+
+end
